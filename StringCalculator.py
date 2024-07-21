d442900ec1d0223e86c0ecbfd3e8b104a1010247
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-22 05:12:26 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -9,4 +9,4 @@ def add(numbers: str) -> int:
     if ifEmptyString(numbers) == 0:
         return 0
     num_list = numbers.split(",")
-    return sum(int(num) for num in num_list)
+    return sum(int(num) for num in num_list if int(num) <= 1000)
