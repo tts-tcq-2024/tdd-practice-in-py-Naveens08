49f4665a18f00202dd8bec5bf5e34a3d897fcc81
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-22 05:25:22 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -5,8 +5,18 @@ def ifEmptyString(numbers: str) -> int:
         return 0
     return -1
 
+def ifNumberLessThanThousand(numbers: str) -> int:
+    if int(num) <= 1000:
+        return 0
+    return -1
+
+def sumNumList(num_list: list) -> int:
+    if ifNumberLessThanThousand == 0:
+        return sum(int(num) for num in num_list)
+    return -1
+
 def add(numbers: str) -> int:
     if ifEmptyString(numbers) == 0:
         return 0
     num_list = numbers.split(",")
-    return sum(int(num) for num in num_list if int(num) <= 1000)
+    return sumNumList(num_list)
