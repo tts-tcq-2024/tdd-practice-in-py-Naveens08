8f41759078327463be5fbcfbf2aa3bd0c708946c
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-22 05:18:45 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -9,30 +9,30 @@ def add(self, numbers: str) -> int:
         return self._sum_numbers(num_list)
 
 def _extract_delimiter(self, numbers: str) -> str:
-default_delimiter = ','
-if numbers.startswith('//'):
-    delimiter_part, numbers = numbers.split('\n', 1)
-    delimiter = delimiter_part[2:]
-    if delimiter.startswith('[') and delimiter.endswith(']'):
-        delimiter = re.escape(delimiter[1:-1])
-    return delimiter
-return default_delimiter
+        default_delimiter = ','
+        if numbers.startswith('//'):
+            delimiter_part, numbers = numbers.split('\n', 1)
+            delimiter = delimiter_part[2:]
+            if delimiter.startswith('[') and delimiter.endswith(']'):
+                delimiter = re.escape(delimiter[1:-1])
+            return delimiter
+        return default_delimiter
 
 def _normalize_numbers(self, numbers: str, delimiter: str) -> str:
-if numbers.startswith('//'):
-    _, numbers = numbers.split('\n', 1)
-return numbers.replace('\n', delimiter)
+        if numbers.startswith('//'):
+            _, numbers = numbers.split('\n', 1)
+        return numbers.replace('\n', delimiter)
 
 def _sum_numbers(self, num_list: list) -> int:
-total = 0
-negatives = []
+        total = 0
+        negatives = []
 
-for num in num_list:
-    if num:
-        number = int(num)
-        if number < 0:
-            negatives.append(number)
-        elif number <= 1000:
-            total += number
-
-return total
+        for num in num_list:
+            if num:
+                number = int(num)
+                if number < 0:
+                    negatives.append(number)
+                elif number <= 1000:
+                    total += number
+        
+        return total
