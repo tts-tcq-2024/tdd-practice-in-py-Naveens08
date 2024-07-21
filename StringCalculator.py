54b053fa4f577dba22b4d39c0c36b7e36ae4e5a9
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-22 04:44:45 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,3 +1,8 @@
-def add(numbers: str) -> int:
+def ifEmptyString(numbers)
     if not numbers:
+        return 0;
+
+def add(numbers: str) -> int:
+    if not ifEmptyString(numbers):
         return 0
+    return -1
