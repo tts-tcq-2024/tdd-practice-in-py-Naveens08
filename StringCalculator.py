8392c2bf241a3d3c15a5b3c6344171a55ebb15e5
Naveens08 <60317324+Naveens08@users.noreply.github.com>
2024-07-22 05:34:56 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,3 +1,5 @@
+import re
+
 def ifEmptyString(numbers: str) -> int:
     if not numbers:
         return 0
@@ -5,11 +7,16 @@ def ifEmptyString(numbers: str) -> int:
         return 0
     return -1
 
+def extract_numbers(string):
+    numbers = re.findall(r'\d+', string)
+    numbers = list(map(int, numbers))
+    return numbers
+
 def sumNumList(num_list: list) -> int:
     return sum(int(num) for num in num_list if int(num) <= 1000)
 
 def add(numbers: str) -> int:
     if ifEmptyString(numbers) == 0:
         return 0
-    num_list = numbers.split(",")
+    num_list = extract_numbers(numbers)
     return sumNumList(num_list)
